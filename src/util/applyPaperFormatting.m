6e120efc289b848b60b045841121d3716948607f
Lukas <l.widmer@gmail.com>
2018-07-24 15:37:27 +0000

    Update applyPaperFormatting.m

--- src/util/applyPaperFormatting.m
+++ src/util/applyPaperFormatting.m
@@ -1,5 +1,5 @@
 function applyPaperFormatting()
-    fig=gcf;
-    set(findall(fig,'-property','FontSize'),'FontSize',12) 
-    set(findall(fig,'-property','FontName'),'FontName','Helvetica')
+    fig = gcf;
+    set(findall(fig, '-property', 'FontSize'), 'FontSize', 12) 
+    set(findall(fig, '-property', 'FontName'), 'FontName', 'Helvetica')
 end
