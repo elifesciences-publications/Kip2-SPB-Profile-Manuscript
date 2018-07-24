ed3c1a2e00984b40a260ccb209eda047e2c37a16
Lukas <l.widmer@gmail.com>
2018-07-24 15:43:32 +0000

    Update applyPaperFormatting.m

--- src/util/applyPaperFormatting.m
+++ src/util/applyPaperFormatting.m
@@ -1,4 +1,7 @@
 function applyPaperFormatting()
+% applyPaperFormatting: Make everything Helvetica, font size 12
+
+% © 2018, ETH Zurich, Lukas Widmer (l.widmer@gmail.com)
     fig = gcf;
     set(findall(fig, '-property', 'FontSize'), 'FontSize', 12) 
     set(findall(fig, '-property', 'FontName'), 'FontName', 'Helvetica')
