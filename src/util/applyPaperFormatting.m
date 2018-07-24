0eb4a03350455f379ca2e9e25a53eccbbb7401c1
Lukas <l.widmer@gmail.com>
2018-07-24 15:36:30 +0000

    Update applyPaperFormatting.m

--- src/util/applyPaperFormatting.m
+++ src/util/applyPaperFormatting.m
@@ -1,5 +1,3 @@
 fig=gcf;
 set(findall(fig,'-property','FontSize'),'FontSize',12) 
 set(findall(fig,'-property','FontName'),'FontName','Helvetica') 
-%set(gcf,'color','none')
-%set(findobj(gcf,'type','axes'),'color','none')
